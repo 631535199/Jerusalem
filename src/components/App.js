92a7f12467cbc459011c43fa73aa8f16a4f910ba
631535199 <631535199@qq.com>
2018-04-20 10:13:13 +0800

    2018.4.20

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jh on 2018/4/20.
+ */
+import React,{Component} from 'react';
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                App
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 /**
  * Created by jh on 2018/4/20.
  */
-var aaa
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App';
+
+render(<App/>,document.querySelector('#root'));
